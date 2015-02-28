ff53048c745f654e3fe32aaa3224a38858e532f5
Philo <philovankemenade@gmail.com>
2015-02-28 15:43:50 +0000

    added tabletop.js

--- script/scrollstories.js
+++ script/scrollstories.js
@@ -1,3 +1,21 @@
+window.onload = function() { 
+	init();
+	scrollStory();
+};
+
+var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Co_byTntKETyuML9aZzfvUiX7HoPQA-AiC_U2Vm6380/pubhtml';
+
+function init() {
+Tabletop.init( { key: public_spreadsheet_url,
+                 callback: showInfo,
+                 simpleSheet: true } )
+}
+
+function showInfo(data, tabletop) {
+alert("Successfully processed!")
+console.log(data);
+}
+
 function scrollStory (argument) {
 	console.log("scrollStory!");
 	$('ul > li').each(function(i, element) {
@@ -11,4 +29,5 @@ function scrollStory (argument) {
 	});
 }
 
-scrollStory();
+
+
