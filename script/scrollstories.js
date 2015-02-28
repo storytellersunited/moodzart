8666a3d09c746ca9ef9e4a9e0f8646be79948a75
Philo <philovankemenade@gmail.com>
2015-02-28 17:41:36 +0000

    filter stories by mood

--- script/scrollstories.js
+++ script/scrollstories.js
@@ -1,19 +1,24 @@
 
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1Co_byTntKETyuML9aZzfvUiX7HoPQA-AiC_U2Vm6380/pubhtml';
+// var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1MwOgW4TIbLq1uDg2_UP3bPKD5AZSnZuFy3Lm3AvkVT8/pubhtml';
+var sheetData;
 
 function getData() {
+	console.log("getData!!!");
 Tabletop.init( { key: public_spreadsheet_url,
                  callback: populateData,
                  simpleSheet: true } )
 }
 
-function showInfo(data, tabletop) {
-	console.log(data[1]["Text"]);
-}
-
 function populateData (data, tabletop) {
+	sheetData = data;
+	console.log("populateData!!!");
+	console.log(mood);
 	for (var i = 0; i < data.length; i++) {
-		$('ul').append('<li class="inactive">'+data[i]['Text']+'</li>')
+		// console.log(data[i]['Text']);
+		if (data[i]['Mood'] === String(mood)) {
+			$('ul').append('<li class="inactive">'+data[i]['Text']+'</li>')	
+		};
 	};
 	scrollStory();
 }
@@ -26,4 +31,17 @@ function scrollStory (argument) {
 	$('ul#storylist > li').each(function(i, element) {
 	    $(element).delay(2000 + (i * 2000)).fadeOut();
 	});
+}
+
+function repopulateData (mood) {
+	console.log("repopulateData!!!");
+	console.log(mood);
+	$('ul#storylist').empty();
+	for (var i = 0; i < sheetData.length; i++) {
+		// console.log(sheetData[i]['Text']);
+		if (sheetData[i]['Mood'] === String(mood)) {
+			$('ul').append('<li class="inactive">'+sheetData[i]['Text']+'</li>')	
+		};
+	};
+	scrollStory();
 }
