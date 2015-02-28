a641b4f3dea2b86956cfbd3697a0d48536be0f8e
dtheio <dpg@MacBook-Air.local>
2015-02-28 15:41:10 +0000

    cleanup

--- script/filter.js
+++ script/filter.js
@@ -2,28 +2,14 @@
 var mood = 2;
 
 function getMood(){
-
-
-
-document.querySelector('#range-mood').addEventListener('change', function(e){
-
-	mood = this.value;
-// 	console.log(mood);
-
-	setMood();
-
-});
-
-
+	document.querySelector('#range-mood').addEventListener('change', function(e){
+		mood = this.value;
+		setMood();
+	});
 }
-
 
 function setMood(){
-	
-	
 		document.getElementsByTagName('body')[0].setAttribute('class', 'mood'+mood);
-
 }
-
 
 setMood();
